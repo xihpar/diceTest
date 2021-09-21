5af8caa255b1e440eab5b42b5f8665a766fc0da4
xihpar <xihpar@users.github.com>
2021-09-21 18:00:07 +0200

    first commit

---
+++ rollDice/src/main/java/com/example/rolldice/Roll.kt
@@ -0,0 +1,32 @@
+package com.example.rolldice
+
+
+
+fun D4(): Int{
+    val result: Int = (1..4).random()
+    return result
+}
+fun D6(): Int{
+    val result: Int = (1..6).random()
+    return result
+}
+fun D8(): Int{
+    val result: Int = (1..8).random()
+    return result
+}
+fun D10(): Int{
+    val result: Int = (1..10).random()
+    return result
+}
+fun D12(): Int{
+    val result: Int = (1..12).random()
+    return result
+}
+fun D20(): Int{
+    val result: Int = (1..20).random()
+    return result
+}
+fun D100(): Int{
+    val result: Int = (1..100).random()
+    return result
+}
